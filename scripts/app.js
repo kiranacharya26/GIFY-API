9b1ef7baa86ecfd9f9b9fa852c704d4a5fd5577d
kiranAch <ach@kiranAchs-Air.bbrouter>
2021-09-19 01:18:18 +0530

    added styles

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('.form-container')
 const collectGIF = document.querySelector('.api-res-cont')
+const closeButton = document.querySelector('.show')
 // const gifDom = document.querySelector('.gif-dom')
 
 const updateUI = (data) => {
   const gifData = data.gifData
+  console.log(gifData)
   let html = ''
 
   gifData.data.map((query) => {
@@ -30,4 +32,12 @@ container.addEventListener('submit', (e) => {
 })
 collectGIF.addEventListener('click', (e) => {
   document.querySelector('.gif-dom').setAttribute('src', e.target.src)
+})
+
+closeButton.addEventListener('click', (e) => {
+  if (!e.target) {
+    container.classList.add('show')
+  } else if (e.target) {
+    container.classList.add('remove')
+  }
 })
